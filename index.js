e8c86240b41660ac0164dfedc42677e16b8dd4c3
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-10 19:33:30 +0600

    set data in mongo db and load services data

--- index.js
+++ index.js
@@ -12,11 +12,23 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vep38nb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const serviceCollection = client.db('foodCurls').collection('services')
+
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services)
+    })
+  }
+  finally {
+
+  }
+}
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
