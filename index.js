c5ef840a60d9878c01711b18e60d0244b15ce28a
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-10 23:21:43 +0600

    specific service card data has been added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vep38nb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -20,9 +20,24 @@ async function run() {
     app.get('/services', async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services)
+    });
+
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services)
+    });
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const services = await serviceCollection.findOne(query);
+      res.send(services);
     })
+
   }
   finally {
 
