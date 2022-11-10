73e57d20a34b87e0ab951bafbade9fca62a0329a
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-10 19:15:03 +0600

    node mongo setup has beed added on server

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 //middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vep38nb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Food Curls Server Is Running')
